e6c88bdf5090db6a7bc0d3bd4a1ee747f5ced93a
S1x6 <v.korshunov@g.nsu.ru>
2017-02-17 00:37:40 +0700

    Update subsystem.h

--- subsystem.h
+++ subsystem.h
@@ -2,5 +2,10 @@
 #define _SUBSYSTEM_H
 #include "parsing.h"
 
+void decode(Input* in);
+void breakStr(int* n, int devider, FILE* out);
+int readBytes(char* dest, int count, int ign, FILE* file);
+char getNumFromB64(char c);
+
 
 #endif
